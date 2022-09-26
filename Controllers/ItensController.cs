113310c42b7b1c7bfdbace40e0e949a57df60897
Guilhermeandrade02 <guilhermeandrade02@hotmail.com>
2022-09-26 16:02:43 -0300

    novo 14

--- Controllers/ItensController.cs
+++ Controllers/ItensController.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 //colocar onde está na pasta
 namespace Varejo.Controller
-{//teste novo
+{//teste novo;;;
     [ApiController]
     [Route("api/user")]
     public class UserControler : ControllerBase
